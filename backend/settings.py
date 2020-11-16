6ab2e49ffe4d84156bf832c386d1ca68c9b673f0
meat9 <meat9@yandex.ru>
2020-11-16 22:01:19 +0300

    rev2

--- backend/settings.py
+++ backend/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "*$dcqleo8f-o)jy4=e*l3@$e&r2-p*t-!l(b&ve8qyi2exm)z4"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*", "84.201.179.160", "www.me91at.cf", "me91at.cf"]
 
@@ -142,12 +142,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # STATIC_URL = "/static/"
 # # STATIC_ROOT = os.path.join(BASE_DIR, "static")
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 # Login
 LOGIN_URL = "/auth/login/"
 LOGIN_REDIRECT_URL = "index"
